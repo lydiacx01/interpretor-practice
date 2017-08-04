4d27b88455d133d02479bd125d0bb6cd4b226fd7
lydiacx01 <lydiacx_develop@outlook.com>
2017-08-04 15:31:54 +0800

    revise calculator with clear whitespace handler and multi-divid-plus-minus-handler

--- interpretor/p4_calculator.py
+++ interpretor/p4_calculator.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'calculat + or - for int'
+'整数的计算器，得到的结果是浮点值，四则运算（不带括号）'
 
 __author__ = 'lydiacx_develop@outlook.com'
 
+'''
+    BNF（巴科斯）范式定义语法如下：
+     term: integer ===> self._term
+     multi-divid-exp: ((multi|divid)term)* ===> self._multi_divid_handler
+     multi-divid-full-exp: term(multi-divid-exp) ===> self.expr中的_multi_divid_handler调用
+     plus-minus-exp: ((plus|minus)(multi-divid-full-exp)+)* ===> self._plus_minus_handler
+     exp: term(plus-minus-exp | multi-divid-exp) ===> self.expr 
+'''
 
 INTEGER, PLUS, MINUS, MULTI, DIVID, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MULTI', 'DIVID', 'EOF'
 PRO_TERM, PRO_PLUS_MINUS, PRO_MULTI_DIVID, PRO_OPERATOR =1, 10, 20, 99
@@ -21,12 +29,12 @@ class Token(object):
         #   Token (INTEGER, 2)
         #   Token (PLUS, '+')
         #   Tokem (EOF, None)
-        
+
         return 'Token ({type}, {value}, {p})'.format(
             type = self.__type,
             value = repr(self.__value),
             p = self.__priority)
-    
+
     __repr__ = __str__
 
     @property
@@ -39,20 +47,19 @@ class Token(object):
     def priority(self):
         return self.__priority
 
-
 class Interpretor(object):
     def __init__(self, text):
         self.text = text
-        self.pos = 0
-        self.current = self.text[self.pos]
-        self.token = None
+        self.current = None
+        self.nextpos = 0
+        self.next = self.text[self.nextpos]
 
     def __str__(self):
-        return 'text={0}, pos={1}, current={2}, token={3}'.format(
+        return 'text={0}, pos={1}, token={2}, next={3}'.format(
                 self.text,
-                self.pos,
+                self.nextpos,
                 repr(self.current),
-                repr(self.token)
+                repr(self.next)
                 )
 
     __repr__ = __str__
@@ -60,29 +67,28 @@ class Interpretor(object):
     def error(self):
         raise Exception('parsing error')
 
+    # the next pointer moves a step
     def _advance(self):
-        self.pos += 1
-        if self.pos >= len(self.text):
-            self.current = None
+        self.nextpos += 1
+        if self.nextpos >= len(self.text):
+            self.next = None
         else:
-            self.current = self.text[self.pos]
+            self.next = self.text[self.nextpos]
 
     def _skip_whitespace(self):
-        while(self.current is not None
-                and self.current.isspace()):
+        while(self.next is not None and self.next.isspace()):
             self._advance()
 
 
     # get next pos of string and format as a token
     def _get_next_token(self):
-        if (self.current is not None):
-            self._skip_whitespace()
-            txt = self.current
-            # end with whitespace
-            if (txt is None):
-                return 
-            elif (txt.isdigit()):
-                self.token = self._int(txt)
+        self._skip_whitespace()
+        
+        if (self.next is not None):
+            txt = self.next
+
+            if (txt.isdigit()):
+                self.current = self._int(txt)
                 return
             # operator like - + * /
             elif (txt == '+'):
@@ -95,90 +101,54 @@ class Interpretor(object):
                 token = Token(DIVID, txt, PRO_MULTI_DIVID)
             else:
                 self.error()
-            
             self._advance()
         else:
             token = None
-        self.token = token
+        self.current = token
     
-    
-    def _operator_check (self, priority):
-        t = self.token
+
+    # get a continuous int string
+    def _int(self, t):
+        while (True):
+            self._advance()
+            if (self.next is None or (not self.next.isdigit())):
+                break
+            t += self.next
+      
+        return Token(INTEGER, t, PRO_TERM)
+        
+    def _operator_check(self, priority):
+        t = self.current
         ty = t.type
         if (priority == PRO_OPERATOR):
             return ty == PLUS or ty == MINUS or ty == MULTI or ty == DIVID
         else:
             return t.priority == priority
-    
+
     ## check current token and get the next token, current token from previous char
     def _eat(self, token_type_priority):
         if (
-                self.token is not None 
-                and 
-                (self.token.type == token_type_priority
-                or 
+                self.current is not None
+                and
+                (self.current.type == token_type_priority
+                or
                 self._operator_check(token_type_priority))
             ):
                 self._get_next_token()
         else:
             self.error()
 
-    # get a continuous int string
-    def _int(self, t):
-        self._advance()
-        while (self.current is not None and self.current.isdigit()):
-            t += self.current
-            self._advance()
-        return Token(INTEGER, t, PRO_TERM)
-    
-    
-    def _term(self):
-        t = self.token
-        if (t is None):
-            self.error()
 
+    def _term(self):
+        t = self.current
         self._eat(INTEGER)
         return int(t.value)
 
-
-    ## known an operator is * or /, then go this cycle to run * or / calculate
-    def _multi_divid(self, term, opt):
-        while (True):
-            right = self._term()
-            if (opt.type == MULTI):
-                term *= right
-            else:
-                term /= right
-            
-            opt = self.token
-            if ((opt is None) or (not self._operator_check(PRO_MULTI_DIVID))):
-                if (opt is not None):
-                    self._eat(PRO_PLUS_MINUS)
-                break
-            else:
-                self._eat(PRO_MULTI_DIVID)
-        
-
-        return (term, opt)
-
-    ## known an operator is + or -, check next operator whether it's * or /
-    def _plus_minus(self, term, opt):
-        while (True):
-            right = self._term()
-            nextOpt = self.token
-            # if there is a next operator existed, do next-check; else just calculate left and right
-            if (self.token is not None):
-                nextOpt = self.token
-                self._eat(PRO_OPERATOR)
-                if (nextOpt.priority == PRO_MULTI_DIVID):
-                    right, nextOpt = self._multi_divid(right, nextOpt)
-
-            term = self._plus_minus_token_calc(opt, term, right)
-            opt = nextOpt
-            if (opt is None):
-                break
-        return (term, opt)
-        
+    def _multi_divid_token_calc(self, token_type, left, right):
+        if (token_type == MULTI):
+            return left * right
+        else:
+            return left / right
 
     def _plus_minus_token_calc(self, token, left, right):
         if token.type == PLUS:
@@ -187,26 +157,49 @@ class Interpretor(object):
             left -= right
         return left
 
+    def _plus_minus_handler(self, left):
+        while(True):
+            opt = self.current
+            self._eat(PRO_PLUS_MINUS)
+            right = self._term()
+
+            if (self.current is None):
+                left = self._plus_minus_token_calc(opt, left, right)
+                break
+            else:
+                if (self._operator_check(PRO_MULTI_DIVID)):
+                    right = self._multi_divid_handler(right)
+                left = self._plus_minus_token_calc(opt, left, right)
+        return left
+
+    def _multi_divid_handler(self, left):
+        while(True):
+            opt = self.current
+            self._eat(PRO_MULTI_DIVID)
+            right = self._term()
+            left = self._multi_divid_token_calc(opt.type, left, right)
+            if ((self.current is None) or (not self._operator_check(PRO_MULTI_DIVID))):
+                break
+        return left
+
+            
 
     # analyze the text
     def expr(self):
         self._get_next_token()
         left = self._term()
-        opt = self.token
-        if (opt is None):
-            return left
 
-        self._eat(PRO_OPERATOR)
-        while (opt is not None):
-            if (opt.priority == PRO_MULTI_DIVID):
-                left, opt = self._multi_divid(left, opt)
+        while (self.current is not None):
+            # begin with * or /
+            if (self._operator_check(PRO_MULTI_DIVID)):
+                left = self._multi_divid_handler(left)
+            # begin with + or -
             else:
-                left, opt = self._plus_minus(left, opt)
-
-
-        return left
-        
+                left = self._plus_minus_handler(left)
     
+        return left
+
+
 
 def test():
     while True:
@@ -218,6 +211,8 @@ def test():
             continue
 
         itp = Interpretor(txt)
+        res = itp.expr()
+        print('done! {0} = {1}'.format(txt, res))
 
 if __name__ == '__main__':
     test()
